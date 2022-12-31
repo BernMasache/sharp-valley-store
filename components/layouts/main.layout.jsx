6e608e36ffef6cab887f5a4f214826ee956dbc4d
BernMasache <bernardmasache@gmail.com>
2022-12-31 22:13:34 +0200

    responsive

--- components/layouts/main.layout.jsx
+++ components/layouts/main.layout.jsx
@@ -131,9 +131,7 @@ export default function MainLayout({ children }) {
                                         {/* Currency selector */}
                                         <form>
                                             <div className="inline-block">
-                                                <label htmlFor="mobile-currency" className="sr-only">
-                                                    Currency
-                                                </label>
+                                                
                                                 <div className="group relative -ml-2 rounded-md border-transparent focus-within:ring-2 focus-within:ring-white">
                                                     <h1 className='text-white'>
                                                         Sharp Valley
@@ -261,7 +259,7 @@ export default function MainLayout({ children }) {
                             >
                                 <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-12 shadow-xl">
                                     <div className="flex items-center justify-between px-4">
-                                        <h2 className="text-lg font-medium text-gray-900">Filters</h2>
+                                        <h2 className="text-lg font-medium text-gray-900">Categories</h2>
                                         <button
                                             type="button"
                                             className="-mr-2 flex h-10 w-10 items-center justify-center rounded-md bg-white p-2 text-gray-400"
