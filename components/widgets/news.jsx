16d1ed9c67ba0cea5bb02026811f577d1a6cec78
BernMasache <bernardmasache@gmail.com>
2023-01-01 09:10:06 +0200

    news layout

--- components/widgets/news.jsx
+++ components/widgets/news.jsx
@@ -1,28 +1,40 @@
-import { BoltIcon, EnvelopeIcon, GlobeAltIcon, ScaleIcon } from '@heroicons/react/24/outline'
+import { BoltIcon, EnvelopeIcon, GlobeAltIcon, ScaleIcon, NewspaperIcon } from '@heroicons/react/24/outline'
+import Link from 'next/link'
 
 const features = [
   {
-    name: 'Competitive rates',
+    name: 'Sharp Valley Spreads Christmas Cheer at QE2 Hospital Blantyre',
     description: 'Consequuntur omnis dicta cumque, inventore atque ab dolores aspernatur tempora ab doloremque.',
-    icon: GlobeAltIcon,
+    icon: NewspaperIcon,
+    imageSrc:"./news/Screen-Shot-2019-02-13-at-6.39.38-PM-600x424.png",
+    href:"https://www.nyasatimes.com/sharp-valley-holdings-spread-christmas-cheers-childrens-ward-queens-hospital"
   },
   {
-    name: 'No hidden fees',
+    name: 'African Economic Outlook 2019',
     description:
       'Corporis quisquam nostrum nulla veniam recusandae temporibus aperiam officia incidunt at distinctio ratione.',
-    icon: ScaleIcon,
+    icon: NewspaperIcon,
+    imageSrc:"./news/Screen-Shot-2019-02-13-at-6.44.59-PM-600x517.png",
+    href:"http://https//www.afdb.org/fileadmin/uploads/afdb/Documents/Publications/2019AEO/AEO_2019-EN.pdf"
+
   },
   {
-    name: 'Instant transfers',
+    name: 'CIA The World Fact Book on Malawi',
     description:
       'Omnis, illo delectus? Libero, possimus nulla nemo tenetur adipisci repellat dolore eligendi velit doloribus mollitia.',
-    icon: BoltIcon,
+    icon: NewspaperIcon,
+    imageSrc:"./news/cia.jpg",
+    href:"https://www.cia.gov/library/publications/resources/the-world-factbook/geos/mi.html"
+
   },
   {
-    name: 'Reminder emails',
+    name: '2018 Malawi Population and Housing Census',
     description:
       'Veniam necessitatibus reiciendis fugit explicabo dolorem nihil et omnis assumenda odit? Quisquam unde accusantium.',
-    icon: EnvelopeIcon,
+    icon: NewspaperIcon,
+    imageSrc:"./news/Census-600x438.png",
+    href:"http://www.nsomalawi.mw/images/stories/data_on_line/demography/census_2018/2018%20Population%20and%20Housing%20Census%20Preliminary%20Report.pdf"
+
   },
 ]
 
@@ -54,43 +66,54 @@ export default function NewsPage() {
         </div>
       </div>
       <div className="relative mx-auto max-w-7xl py-24 px-6 sm:py-32 lg:px-8 lg:py-40">
-        {/* <svg
-          className="absolute top-0 left-full -translate-x-1/2 -translate-y-3/4 transform lg:left-auto lg:right-full lg:translate-x-2/3 lg:translate-y-1/4"
-          width={404}
-          height={784}
-          fill="none"
-          viewBox="0 0 404 784"
-          aria-hidden="true"
-        >
-          <defs>
-            <pattern
-              id="8b1b5f72-e944-4457-af67-0c6d15a99f38"
-              x={0}
-              y={0}
-              width={20}
-              height={20}
-              patternUnits="userSpaceOnUse"
-            >
-              <rect x={0} y={0} width={4} height={4} className="text-gray-100" fill="currentColor" />
-            </pattern>
-          </defs>
-          <rect width={404} height={784} fill="url(#8b1b5f72-e944-4457-af67-0c6d15a99f38)" />
-        </svg> */}
+
 
         <div className="relative lg:grid lg:grid-cols-3 lg:gap-x-12 xl:gap-x-16">
-          <div className="lg:col-span-1">
-            <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl">News Section.</h2>
+          <div className="lg:col-span-2 mb-8">
+            <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl">News Section</h2>
           </div>
-          <dl className="mt-20 grid grid-cols-1 gap-16 sm:grid-cols-2 sm:gap-x-12 lg:col-span-2 lg:mt-0">
-            {features.map((feature) => (
-              <div key={feature.name}>
-                <dt>
-                  <div className="flex h-12 w-12 items-center justify-center rounded-xl bg-indigo-500 text-white">
-                    <feature.icon className="h-8 w-8" aria-hidden="true" />
+          <dl className="mt-8 grid grid-cols-2 gap-16 sm:grid-cols-1 sm:gap-x-4 lg:col-span-12 lg:mt-0">
+            {features.map((news) => (
+              <div key={news.name}>
+              
+                <div className="bg-white pt-2 lg:py-4">
+                  <div className="bg-sky-700 pb-4 lg:relative lg:z-10 lg:pb-0">
+                    <div className="lg:mx-auto lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-8 lg:px-8">
+                      <div className="relative lg:-my-4">
+                        <div aria-hidden="true" className="absolute inset-x-0 top-0 h-1/2 bg-white lg:hidden" />
+                        <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:h-full lg:p-0">
+                          <div className="aspect-w-16 aspect-h-16 overflow-hidden rounded-xl shadow-xl sm:aspect-w-16 sm:aspect-h-7 lg:aspect-none lg:h-full">
+                            <img
+                              className="object-cover lg:h-full lg:w-full"
+                              src={news.imageSrc}
+                              alt=""
+                            />
+                          </div>
+                        </div>
+                      </div>
+                      <div className="mt-2 lg:col-span-2 lg:m-0 lg:pl-8">
+                        <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0 lg:py-20">
+                          <blockquote>
+                            <div>
+                             
+                              <div className="">
+                                <NewspaperIcon className='h-12 w-12 text-white opacity-75'/>
+                              </div>
+                              <p className="mt-6 text-xl text-white">
+                                {news.name}
+                              </p>
+                            </div>
+                            <footer className="mt-6">
+                             
+                              <Link target="_blank" rel="" href={news.href} className="bg-transparent hover:bg-blue text-gray-100 font-semibold hover:text-gray-300 py-2 px-4 border border-blue hover:border-transparent rounded">Read full story</Link>
+                             
+                            </footer>
+                          </blockquote>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                  <p className="mt-6 text-lg font-semibold leading-8 text-gray-900">{feature.name}</p>
-                </dt>
-                <dd className="mt-2 text-base text-gray-600">{feature.description}</dd>
+                </div>
               </div>
             ))}
           </dl>
